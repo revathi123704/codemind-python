d67bd62a1398f7d3c4950bf7889b1576bf0d08de
revathi123704 <111487960+revathi123704@users.noreply.github.com>
2022-08-18 12:30:06 +0530

    import math as M def isperfect(num):     res=1     i=2     sq=int(M.sqrt(num))     for i in range(2,sq+1):         if num%i==0:             res+=i             res+=num//i     return res==num
    num=int(input())
    res=isperfect(num)
    print(res)

---
+++ Perfect_Number.py
@@ -0,0 +1,16 @@
+import math as M
+def isperfect(num):
+    res=1
+    i=2
+    sq=int(M.sqrt(num))
+    for i in range(2,sq+1):
+        if num%i==0:
+            res+=i
+            res+=num//i
+    return res==num
+
+
+
+num=int(input())
+res=isperfect(num)
+print(res)
